f3eb697db1a37d3acf36ee8b9eaaa21c98e76f49
Florent <bretonremy1@gmail.com>
2024-09-09 19:43:59 -0400

    :sparkles: Call init SQL system, and add base of docker compose

--- web_server/index.js
+++ web_server/index.js
@@ -1,10 +1,13 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
+
+const { initSQL, endSQL } = require('./SQL/init');
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+    res.send('Hello World!')
+})
 
 app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
-});
+    console.log('Server is running on http://localhost:3000')
+    initSQL()
+})
