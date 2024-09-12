617b5452aef89daff0d8e66a923fec76817e261a
Florent <bretonremy1@gmail.com>
2024-09-12 10:30:45 -0400

    :sparkles: Removing global init.sql, now you need to create folder and put init.sql in it

--- web_server/SQL/init.js
+++ web_server/SQL/init.js
@@ -12,27 +12,53 @@ const connection = mysql.createConnection({ // TODO : replace by .env file
 const initSQL = () => {
     connection.connect((err) => {
         if (err) {
-            console.error('Connexion error : ' + err.stack)
-            return
+            console.error('Connection error: ' + err.stack);
+            return;
         }
-        console.log('Connected to DB.')
-    
-        const sqlFilePath = path.join(__dirname, '', 'init.sql');
-        const sql = fs.readFileSync(sqlFilePath, 'utf-8');
-    
-        const queries = sql.split(';').filter(query => query.trim().length > 0);
-    
-        for (const query of queries) {
-            connection.query(query);
-            console.log('Executed query:', query);
-        }
-    
-        console.log('SQL file executed successfully.');
-    })
-}
+        console.log('Connected to DB.');
+
+        const parentDir = path.join(__dirname, '.');
+
+        fs.readdir(parentDir, (err, files) => {
+            if (err) {
+                console.error('Error reading directory:', err);
+                return;
+            }
+
+            const directories = files.filter(file => {
+                return fs.statSync(path.join(parentDir, file)).isDirectory();
+            });
+
+            directories.forEach(directory => {
+                const sqlFilePath = path.join(parentDir, directory, 'init.sql');
+                
+                if (fs.existsSync(sqlFilePath)) {
+                    console.log(`Found init.sql in ${directory}`);
+
+                    const sql = fs.readFileSync(sqlFilePath, 'utf-8');
+                    const queries = sql.split(';').filter(query => query.trim().length > 0);
+
+                    queries.forEach(query => {
+                        connection.query(query, (err) => {
+                            if (err) {
+                                console.error(`Error executing query in ${directory}:`, err);
+                            } else {
+                                console.log(`Executed query in ${directory}:`, query.trim());
+                            }
+                        });
+                    });
+                } else {
+                    console.log(`No init.sql found in ${directory}`);
+                }
+            });
+
+            console.log('SQL execution completed.');
+        });
+    });
+};
 
 const endSQL = () => {
     connection.end()
 }
 
-module.exports = { initSQL, endSQL }
+module.exports = { initSQL, endSQL, connection }
