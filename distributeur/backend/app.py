6b065d7e900f4754ee0fa1dce4fd3762acbb53d1
Erwann-Laplante <91593352+ErwannL@users.noreply.github.com>
2024-10-12 16:45:35 +0200

    add testing routes for the front-end

--- distributeur/backend/app.py
+++ distributeur/backend/app.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 from routes.find_doctor import find_doctor_bp
 from routes.add_doctor import add_doctor_bp
 from routes.remove_doctor import remove_doctor_bp
+from routes.testing_connection_front import testFrontPost_bp, testFrontGet_bp, testFrontDelete_bp, testFrontPut_bp
 
 # Charger les variables d'environnement à partir du fichier .env
 load_dotenv()
@@ -14,6 +15,12 @@ app = Flask(__name__)
 app.register_blueprint(find_doctor_bp)
 app.register_blueprint(add_doctor_bp)
 app.register_blueprint(remove_doctor_bp)
+
+app.register_blueprint(testFrontPost_bp)
+app.register_blueprint(testFrontGet_bp)
+app.register_blueprint(testFrontDelete_bp)
+app.register_blueprint(testFrontPut_bp)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
