b692d191850a90cee1648332d77828bf86cf5559
Florent <bretonremy1@gmail.com>
2024-10-21 14:01:30 -0400

    :sparkles: Add swagger exemple

--- web_server/API/users.js
+++ web_server/API/users.js
@@ -1,9 +1,73 @@
 const express = require('express')
-const app = express
+const router = express.Router()
+const bcrypt = require('bcryptjs')
+const { createUser } = require('../SQL/users/query')
 const { authenticateToken, disableToken, createToken } = require("../tools/token")
 const { doesUserExits } = require("../SQL/users/query")
 
-app.post('/register', (req, res) => {
+/**
+ * @swagger
+ * /auth/register:
+ *   post:
+ *     summary: Inscrit un nouvel utilisateur
+ *     description: Crée un utilisateur avec un mot de passe haché si l'adresse e-mail n'existe pas déjà.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 example: John Doe
+ *                 description: Nom de l'utilisateur.
+ *               password:
+ *                 type: string
+ *                 example: "strongpassword123"
+ *                 description: Mot de passe de l'utilisateur.
+ *               email:
+ *                 type: string
+ *                 example: john.doe@example.com
+ *                 description: Adresse e-mail de l'utilisateur.
+ *               birthdate:
+ *                 type: timestamp
+ *                 format: date
+ *                 example: "1729533390"
+ *                 description: Date de naissance de l'utilisateur.
+ *     responses:
+ *       201:
+ *         description: Utilisateur inscrit avec succès.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: User registered successfully
+ *       400:
+ *         description: L'utilisateur existe déjà.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   example: User already exists
+ *       500:
+ *         description: Échec de la requête à la base de données ou de la création de l'utilisateur.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   example: Database query failed
+*/
+router.post('/register', (req, res) => {
     const { name, password, email, birthdate } = req.body
 
     doesUserExits(email, (err, user) => {
@@ -18,10 +82,11 @@ app.post('/register', (req, res) => {
                 return res.status(500).json({ error: 'Failed to create user' })
             res.status(201).json({ message: 'User registered successfully' })
         })
-    })  
+    })
 })
+
 // TO DO : list of connected user // token
-app.post('/login', (req, res) => {
+router.post('/login', (req, res) => {
     const { password, email } = req.body
 
     doesUserExits(email, (err, user) => {
@@ -37,12 +102,14 @@ app.post('/login', (req, res) => {
 
             res.status(201).json({ message: 'User connected', token: createToken(user[0].email, user[0].id) })
         })
-    })  
+    })
 })
 
-app.post('/logout', authenticateToken, (req, res) => {
+router.post('/logout', authenticateToken, (req, res) => {
     const token = req.headers['authorization']
 
     disableToken(token)
     res.status(201).json({ message: 'User disconnected' })
 })
+
+module.exports = router
--- web_server/index.js
+++ web_server/index.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-
+const swaggerUi = require('swagger-ui-express')
+const swaggerSpec = require('./docs/swagger')
+const authRoutes = require('./API/users')
 const { initSQL, endSQL } = require('./SQL/init')
 
 app.use(bodyParser.json())
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
+app.use('/auth', authRoutes)
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
