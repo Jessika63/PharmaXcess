03e9596f27f0674651d92d64a3be6053ff77dcd5
Erwann-Laplante <91593352+ErwannL@users.noreply.github.com>
2024-10-28 17:44:40 +0100

    add yaml

--- distributeur/backend/scripts/safety/run_safety.sh
+++ distributeur/backend/scripts/safety/run_safety.sh
@@ -2,7 +2,7 @@
 #!/bin/bash
 
 # Exécuter Safety
-safety scan -r ./distributeur/backend/requirements.txt
+safety check -r ./distributeur/backend/requirements.txt --policy-file ./distributeur/backend/scripts/safety/safety-policy.yaml"
 
 # Capturer le code de sortie pour GitHub Actions (0 si OK, 1 s'il y a des vulnérabilités)
 exit $?
