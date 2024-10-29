8dd148170f0f2b2ca57c882b4a728bb5b512ad53
antoine_fld <antoinef@senoee.com>
2024-10-29 16:00:54 +0200

    [ADD] tempiorary starting page example

--- distributeur/frontend/src/App.js
+++ distributeur/frontend/src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+
+      {/* logo PharmaXcess */}
+
+      <img src={require("./assets/logo.png")} alt="Logo PharmaXcess" className="logo"></img>
+
+      {/* page de garde */}
+      <div>
+        {/* rectangle medicaments sous ordonnance */}
+        <div class="rectangle" style={{top: '30%', left: '50%'}}>
+          <p style={{fontSize: '2.5em'}}>
+            Médicaments sous ordonnance
+          </p>
+        </div>
+
+        {/* rectangle medicaments sans ordonnance */}
+        <div class="rectangle" style={{top: '60%', left: '50%'}}>
+          <p style={{fontSize: '2.5em'}}>
+            Médicaments sans ordonnance
+          </p>
+        </div>
+
+      </div>
     </div>
   );
 }
