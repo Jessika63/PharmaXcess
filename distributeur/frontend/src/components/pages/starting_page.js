918272ae4df25db5336a35f902828cb473f8eeed
antoine_fld <antoinef@senoee.com>
2024-10-30 12:26:55 +0200

    [ADD] document_checking page + back button to starting page

--- distributeur/frontend/src/components/pages/starting_page.js
+++ distributeur/frontend/src/components/pages/starting_page.js
@@ -1,15 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../../App.css'
 
 function StartingPage() {
   return (
     <div className="starting_page">
       {/* Rectangle 'médicaments sous ordonnance' */}
-      <div className="rectangle" style={{top: '20%', left: '50%'}}>
-        <p style={{ fontSize: '3em' }}>
-          Médicaments sous ordonnance
-        </p>
-      </div>
+
+      <Link to="/documents-checking" style={{ textDecoration: 'none' }}>
+        <div className="rectangle" style={{top: '20%', left: '50%', cursor: 'pointer'}}>
+          <p style={{ fontSize: '3em' }}>
+            Médicaments sous ordonnance
+          </p>
+        </div>
+      </Link>
 
       {/* Rectangle 'médicaments sans ordonnance' */}
       <div className="rectangle" style={{top: '35%', left: '50%'}}>
