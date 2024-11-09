8ddd3aab6d00fd811d0e1d586db2d20d941fe104
Célian Friedrich <celian.friedrich@epitech.eu>
2024-11-09 10:28:30 +0100

    [DEL] removed mobile backend code for better version

--- app/web_server/SQL/vending_machines/init.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS vending_machines (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    latitude DECIMAL(9,6) NOT NULL,
-    longitude DECIMAL(9,6) NOT NULL,
-    status ENUM('operational', 'under maintenance', 'out of service') DEFAULT 'operational'
-);
