a113e35df7fb63017a4530a7c73f74b804925561
antoine_fld <antoinef@senoee.com>
2024-11-25 13:37:32 +0200

    [ADD] drug unavailable page + drug stores list page + payment failed page

--- distributeur/frontend/src/routes.js
+++ distributeur/frontend/src/routes.js
@@ -3,6 +3,9 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import App from './App';
 import DocumentsChecking from './components/pages/documents_checking';
 import NonPrescriptionDrugs from './components/pages/non_prescription_drugs';
+import DrugUnavailable from './components/pages/drug_unavailable';
+import DrugStoresAvailable from './components/pages/drug_stores_available';
+import PaymentFailed from './components/pages/payment_failed';
 
 function AppRoutes() {
     return (
@@ -10,6 +13,9 @@ function AppRoutes() {
             <Routes>
                 <Route path="/" element={<App />} />
                 <Route path="/documents-checking" element={<DocumentsChecking />} />
+                <Route path="/drug-unavailable" element={<DrugUnavailable />} />
+                <Route path="/drug-stores-available" element={<DrugStoresAvailable />} />
+                <Route path="/payment-failed" element={<PaymentFailed />} />
                 <Route path="/non-prescription-drugs" element={<NonPrescriptionDrugs />} />
             </Routes>
         </Router>
