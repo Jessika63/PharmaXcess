dbbc5e22fe72809a6365b546bee06c31d15b0a52
antoinefld <antoine.fillaudeau@epitech.eu>
2025-03-31 12:00:01 +0000

    [FIX] responsive & sizes + docker compose update for hot reload

--- distributeur/frontend/src/components/pages/drug_unavailable.js
+++ distributeur/frontend/src/components/pages/drug_unavailable.js
@@ -44,30 +44,29 @@ function DrugUnavailable() {
   };  
 
   return (
-
     <div className="bg-background_color w-full h-screen flex flex-col justify-center items-center">
 
         {/* Header */}
-        <div className="w-4/5 h-48 flex justify-center items-center mb-8 mt-10">
+        <div className="w-4/5 h-36 flex justify-center items-center mt-4">
             {/* Logo */}
             <div className="flex justify-center items-center w-full">
-                <img src={require('./../../assets/logo.png')} alt="Logo PharmaXcess" className="w-124 h-32" />
+                <img src={require('./../../assets/logo.png')} alt="Logo PharmaXcess" className="w-76 h-24" />
             </div>
         </div>
 
-        {/* Rectangle message d'information */}
-        <div className="w-3/4 bg-background_color p-8 rounded-xl  text-center mb-12">
-            <p className="text-5xl text-gray-800">
+        {/* Container Information Message */}
+        <div className="w-2/3 bg-background_color p-2 rounded-xl text-center mb-6">
+            <p className="text-3xl text-gray-800">
               Le médicament que vous voulez n'est plus disponible.<br />
               Voici nos propositions pour résoudre ce problème:
             </p>
         </div>
 
-        {/* Container pour center les 2 boutons */}
-          <div className="flex flex-col items-center space-y-16 w-full">
+        {/* Container for centering both buttons */}
+        <div className="flex flex-col items-center space-y-10 w-full">
       
-              {/* Bouton 'Commander plus tard' */}
-              <div 
+            {/* Button 'Commander plus tard' */}
+            <div 
                 tabIndex={0} 
                 ref={order_later}
                 onClick={() => navigate('/' + (location.state?.from ?? '#'))} 
@@ -78,16 +77,17 @@ function DrugUnavailable() {
                       navigate('/' + (location.state?.from ?? '#'))
                     }
                 }}
-                className={`w-1/2 h-72 flex items-center justify-center rounded-3xl shadow-lg 
-                    bg-gradient-to-r from-pink-500 to-rose-400 text-gray-800 text-5xl cursor-pointer
+                className={`w-2/5 h-36 flex items-center justify-center text-center rounded-2xl shadow-lg 
+                    bg-gradient-to-r from-pink-500 to-rose-400 text-gray-800 text-3xl cursor-pointer
                     transition-transform duration-500 hover:from-[#d45b93] focus:ring-opacity-50
                     hover:to-[#e65866] hover:scale-105 focus:ring-4 focus:ring-pink-500
-                    ${focusedIndex === 0 ? 'scale-105' : ''}`}>
+                    ${focusedIndex === 0 ? 'scale-105' : ''}`}
+            >
                 Commander le médicament et le récupérer plus tard
-              </div>
+            </div>
       
-              {/* Bouton 'Liste des pharmacies' */}
-              <div
+            {/* Button 'Liste des pharmacies' */}
+            <div
                 onClick={() => navigate('/drug-stores-available', { state: { from: 'drug-unavailable' } })}
                 onKeyDown={(event) => {
                   if (event.key === "Enter") {
@@ -97,19 +97,19 @@ function DrugUnavailable() {
                 }}
                 tabIndex={0}
                 ref={drug_store_list}
-                className={`w-1/2 h-72 flex items-center justify-center rounded-3xl shadow-lg 
-                    bg-gradient-to-r from-pink-500 to-rose-400 text-gray-800 text-5xl
+                className={`w-2/5 h-36 flex items-center justify-center text-center rounded-2xl shadow-lg 
+                    bg-gradient-to-r from-pink-500 to-rose-400 text-gray-800 text-3xl
                     transition-transform duration-500 hover:from-[#d45b93] cursor-pointer
                     hover:to-[#e65866] hover:scale-105 focus:ring-2 focus:ring-pink-500
                     ${focusedIndex === 1 ? 'scale-105' : ''}`}
-              >
+            >
                 Liste des pharmacies possédant le médicament
-              </div>
+            </div>
 
-      
-          </div>
+        </div>
     </div>
   );
+
 }
 
 export default DrugUnavailable;
