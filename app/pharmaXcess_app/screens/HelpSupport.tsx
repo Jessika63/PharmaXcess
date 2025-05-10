e13e2724fccc422bfca31ab6b4c418916b63ee77
Jess <jessica.ebely@epitech.eu>
2025-05-11 03:05:36 +0900

    add all the different help support options

--- app/pharmaXcess_app/screens/HelpSupport.tsx
+++ app/pharmaXcess_app/screens/HelpSupport.tsx
@@ -5,17 +5,17 @@ import Ionicons from '@expo/vector-icons/Ionicons';
 
 interface Item {
     title: string;
-    description: string;
+    route: string;
     icon: React.ComponentProps<typeof Ionicons>['name'];
 }
 
 export default function HelpSupport({ navigation }): JSX.Element {
 
     const items: Item[] = [
-        { title: 'Aide et support', description: 'Obtenez de l\'aide et du support pour votre application.', icon: 'help-circle-outline' },
-        { title: 'Contactez-nous', description: 'Contactez notre équipe d\'assistance.', icon: 'mail-outline' },
-        { title: 'FAQ', description: 'Consultez notre foire aux questions.', icon: 'information-circle-outline' },
-        { title: 'Retour d\'expérience', description: 'Partagez vos commentaires sur l\'application.', icon: 'chatbubble-ellipses-outline' },
+        { title: 'Tutoriel', route: 'Tutorial', icon: 'book-outline' },
+        { title: 'FAQ', route: 'FAQ', icon: 'help-circle-outline' },
+        { title: 'Assistance technique', route: 'TechnicalSupport', icon: 'construct-outline' },
+        { title: 'Signaler un problème', route: 'ReportIssue', icon: 'alert-circle-outline' },
     ];
 
     return (
@@ -56,61 +56,16 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         borderRadius: 10,
         alignItems: 'center',
-        flexDirection: 'row',
-        paddingHorizontal: 20,
     },
     itemText: {
         fontSize: 20,
         color: 'white',
-        flex: 1,
-        fontWeight: 'bold',
         marginLeft: 10,
+        fontWeight: 'bold',
     },
     icon: {
         width: 24,
         height: 24,
         marginLeft: 10,
-    },
-    returnButton: {
-        marginTop: 20,
-        borderRadius: 10,
-        width: '100%',
-        overflow: 'hidden',
-    },
-    gradient: {
-        paddingVertical: 15,
-        borderRadius: 5,
-        alignItems: 'center',
-        flexDirection: 'row',
-        justifyContent: 'center',
-        paddingHorizontal: 20,
-    },
-    returnButtonText: {
-        fontSize: 18,
-        color: 'white',
-        fontWeight: 'bold',
-    },
-    option: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        padding: 10,
-        marginBottom: 10,
-        borderRadius: 5,
-        backgroundColor: '#adadad',
-    },
-    optionText: {
-        fontSize: 18,
-        marginLeft: 10,
-        color: 'white',
-        fontWeight: 'bold',
-    },
-    selectedOption: {
-        backgroundColor: '#F57196',
-        borderRadius: 10,
-        padding: 10,
-        flexDirection: 'row',
-        alignItems: 'center',
-        marginBottom: 10,
-        overflow: 'hidden',
     },
 }); 
