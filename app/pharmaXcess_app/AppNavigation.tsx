f292f07b719efaa9ead2eba60c0d34648454d04c
Jess <jessica.ebely@epitech.eu>
2025-05-17 23:18:34 +0900

    create medical profile tutorial screen

--- app/pharmaXcess_app/AppNavigation.tsx
+++ app/pharmaXcess_app/AppNavigation.tsx
@@ -40,6 +40,7 @@ import Tutorial from './screens/Tutorial';
 import FirstSteps from './screens/FirstSteps';
 import MedicationManagement from './screens/MedicationManagement';
 import PrescriptionImport from './screens/PrescriptionImport';
+import MedicalProfile from './screens/MedicalProfile';
 
 type TabBarIconProps = {
   color: string;
@@ -94,6 +95,7 @@ function ProfileStackScreen(): JSX.Element {
       <ProfileStack.Screen name="FirstSteps" component={FirstSteps} options={{ headerTitleAlign: 'center', title: 'Premiers pas' }} />
       <ProfileStack.Screen name="MedicationManagement" component={MedicationManagement} options={{ headerTitleAlign: 'center', title: 'Gestion des médicaments' }} />
       <ProfileStack.Screen name="PrescriptionImport" component={PrescriptionImport} options={{ headerTitleAlign: 'center', title: 'Importation des ordonnances' }} />
+      <ProfileStack.Screen name="MedicalProfile" component={MedicalProfile} options={{ headerTitleAlign: 'center', title: 'Profil médical' }} />
     </ProfileStack.Navigator>
   );
 }
