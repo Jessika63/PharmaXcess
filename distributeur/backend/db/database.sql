6d3c6e7342d570cd4e63113750cd03e956341b05
Erwann Laplante <erwann.laplante@epitech.eu>
2025-06-30 01:22:03 +0200

    Add table creation in fake dump

--- distributeur/backend/db/database.sql
+++ distributeur/backend/db/database.sql
@@ -1,12 +1,13 @@
 connect db_distributeur
 
-CREATE DATABASE doctors_db;
+CREATE DATABASE IF NOT EXISTS doctors_db;
+USE doctors_db;
 
-CREATE TABLE IF NOT EXISTS "doctors" (
+CREATE TABLE IF NOT EXISTS doctors (
     id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(255) NOT NULL,
-    last_name VARCHAR(255) NOT NULL;
+    last_name VARCHAR(255) NOT NULL,
     rpps_code VARCHAR(50) NOT NULL,
     sector VARCHAR(255) NOT NULL,
-    region VARCHAR(255) NOT NULL;
+    region VARCHAR(255) NOT NULL
 );
