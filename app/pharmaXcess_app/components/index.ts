ecbaebf7558daef52453445c5986734eeb22d41c
Erwann Laplante <erwann.laplante@epitech.eu>
2025-09-19 22:58:39 +0200

    Remove all files from dev_v2 branch

--- app/pharmaXcess_app/components/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as CustomPicker } from './CustomPicker';
-export { default as DatePicker } from './DatePicker';
-export { default as TimePicker } from './TimePicker';
-export { default as QuantityPicker } from './QuantityPicker';
-export { ProfileCard, ProfileManager } from './ProfileManager';
--- distributeur/frontend/src/utils/fetchWithTimeout.js
+++
@@ -1,8 +0,0 @@
-export default function fetchWithTimeout(resource, options = {}, timeout = 5000) {
-  return Promise.race([
-    fetch(resource, options),
-    new Promise((_, reject) =>
-      setTimeout(() => reject(new Error('Timeout')), timeout)
-    )
-  ]);
-} 
