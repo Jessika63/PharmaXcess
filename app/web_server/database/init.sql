fe4248ad0f1898e7ffd3510cb75dcaad64041bbb
Florent <bretonremy1@gmail.com>
2025-10-05 11:32:27 +0200

    [ADD] All missing DB table

--- app/web_server/database/init.sql
+++ app/web_server/database/init.sql
@@ -61,4 +61,74 @@ CREATE TABLE IF NOT EXISTS ordonnances (
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 )
 
+CREATE TABLE IF NOT EXISTS doctors (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    speciality TEXT NOT NULL,
+    phoneNumber TEXT NOT NULL,
+    email TEXT NOT NULL,
+    address TEXT NOT NULL,
+    hospital TEXT NOT NULL,
+)
+
+CREATE TABLE IF NOT EXISTS familyDisability (
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    name TEXT NOT NULL,
+    familyMember TEXT NOT NULL,
+    severity TEXT NOT NULL,
+    treatement TEXT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+)
+
+CREATE TABLE IF NOT EXISTS allergies (
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    name TEXT NOT NULL,
+    beginDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    severity TEXT NOT NULL,
+    symptoms TEXT NOT NULL,
+    medications TEXT NOT NULL,
+    comments TEXT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+)
+
+CREATE TABLE IF NOT EXISTS medicalHistory (
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    name TEXT NOT NULL,
+    description TEXT NOT NULL,
+    beginDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    endDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    department TEXT NOT NULL,
+    hospital TEXT NOT NULL,
+    doctor TEXT NOT NULL,
+    medications TEXT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+)
+
+CREATE TABLE IF NOT EXISTS treatements (
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    name TEXT NOT NULL,
+    beginDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    endDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    dosage TEXT NOT NULL,
+    duration TEXT NOT NULL,
+    sideEffects TEXT NOT NULL,
+    disease TEXT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+)
+
+CREATE TABLE IF NOT EXISTS diseases (
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    name TEXT NOT NULL,
+    description TEXT NOT NULL,
+    beginDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    medications name TEXT NOT NULL,
+    examens name TEXT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+)
+
 CREATE INDEX IF NOT EXISTS idx_location ON machines USING GIST (location);
